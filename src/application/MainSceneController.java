ef3c41ceca6ede5aeeb141c4efc83f4ea9247cee
jonasmachados <jonasmachado.ti@gmail.com>
2020-12-20 10:46:43 -0300

    Create screen About and Class Util

--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -7,21 +7,64 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Alert;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import util.Alerts;
 
 /**
  * FXML Controller class
  *
- * @author Jonas
+ * @author Jonas create 20/12/2020
+ * update 
  */
 public class MainSceneController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML
+    private MenuItem menuItemAbout;
+    
+    //Metodo para tratar os eventos, tratando botao ABOUT
+    @FXML
+    public void onMenuItemAboutAction() {
+        loadView("About.fxml", x -> {});
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }  
     
+    //Criando um FXMLLoader para carregar a tela,e sincronizandod evitando as threads
+    private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); //GETROOT pega o primeiro elemento da View
+
+            //Manipulacao da tela principal
+            Node mainMenu = mainVBox.getChildren().get(0); //Creia um node recebe o Vbox
+            mainVBox.getChildren().clear();//Limpa todos os filhos do Main VBox
+            mainVBox.getChildren().add(mainMenu);//Pega os filhos do Main Nebu
+            mainVBox.getChildren().addAll(newVBox.getChildren()); //Adicionando uma colecao < os filhos do Vbox
+            
+            //Executa a funcao
+            T controller = loader.getController();
+            initializingAction.accept(controller);
+        } catch (Exception e) {
+            //Caso ocorra a execao, o sistema ira msotrar uma Janela de alerta com o Erro
+            Alerts.showAlert("IOException", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+
+    }
 }
+    
+
---
+++ src/util/Utils.java
@@ -0,0 +1,111 @@
+package util;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+public class Utils {
+
+    //Metodo para acessar o Stage onde o Controller que recebeu o evento esta
+    public static Stage currentStage(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+    }
+
+    //Metodo para converter o valor da caixa de texto para Inteiro
+    public static Integer tryParseToInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+    
+        //Metodo para converter o valor da caixa de texto para Inteiro
+    public static Double tryParseToDouble(String  str) {
+        try {
+            return Double.parseDouble(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    //Formatar Table Column Date(Data)
+    public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Date> cell = new TableCell<T, Date>() {
+                private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+                @Override
+                protected void updateItem(Date item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(sdf.format(item));
+                    }
+                }
+            };
+            return cell;
+        });
+    }
+
+    //Formatar Table Column Double(Ponto flutuante)
+    public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Double> cell = new TableCell<T, Double>() {
+                @Override
+                protected void updateItem(Double item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        Locale.setDefault(Locale.US);//Locale formata teclado
+                        setText(String.format("%." + decimalPlaces + "f", item));
+                    }
+                }
+            };
+            return cell;
+        });
+    }
+
+    //Metodo para formatar a Data 
+    public static void formatDatePicker(DatePicker datePicker, String format) {
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+
+            {
+                datePicker.setPromptText(format.toLowerCase());
+            }
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);//PAssa  o localDate para uma String
+                } else {
+                    return null;
+                }
+            }
+        });
+    }
+
+}
